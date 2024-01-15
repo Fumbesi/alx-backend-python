0ccd2e6eef38499307b5a42f33d8928c32f1315c
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-15 04:23:31 -0800

    0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -26,6 +26,7 @@ async def wait_random(max_delay: int = 10) -> float:
 
 
 if __name__ == "__main__":
-    print(asyncio.run(wait_random()))
-    print(asyncio.run(wait_random(5)))
-    print(asyncio.run(wait_random(15)))
+    loop = asyncio.get_event_loop()
+    print(loop.run_until_complete(wait_random()))
+    print(loop.run_until_complete(wait_random(5)))
+    print(loop.run_until_complete(wait_random(15)))
